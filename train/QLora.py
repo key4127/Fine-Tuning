c021b487be93c01d547f3fc9c0890a645629ca59
key4127 <2827092384@qq.com>
2025-09-01 21:21:54 +0800

    complete a QLora script demo

---
+++ train/QLora.py
@@ -0,0 +1,74 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments
+from peft import LoraConfig
+from trl import SFTTrainer
+from datasets import Dataset
+import torch
+import json
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model_path = "./model/Qwen2.5-Coder-1.5B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    quantization_config=bnb_config,
+    device_map="auto"
+)
+model.config.use_cache = False
+
+tokenizer = AutoTokenizer.from_pretrained(
+    model_path,
+    trust_remote_code=True
+)
+tokenizer.pad_token = tokenizer.eos_token
+
+lora_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+data_path = "./data/train/alpaca_data.jsonl"
+formatted_data = []
+
+with open(data_path, "r", encoding="utf-8") as data:
+    for line in data:
+        record = json.loads(line.strip())
+        new_conv = [{"role": item["from"], "content": item["value"]} for item in record]
+        formatted_data.append({"messages": new_conv})
+
+qwen_dataset = Dataset.from_list(formatted_data)
+
+training_args = TrainingArguments(
+    output_dir="./model_output/QLora",
+    num_train_epochs=3,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=4,
+    optim="paged_adamw_8bit",
+    learning_rate=2e-4,
+    fp16=True,
+    logging_steps=100,
+    save_strategy="epoch"
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=qwen_dataset,
+    peft_config=lora_config,
+    max_seq_length=512,
+    tokenizer=tokenizer,
+    args=training_args
+)
+
+print("begin training...")
+trainer.train()
+
+print("save training...")
+trainer.save_model("./model_output_QLora/final1")
